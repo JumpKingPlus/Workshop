a393ec906a7f76f5da473d70748b0fa8a4000f63
Andrea <hello@phoenixx.design>
2024-02-27 23:22:40 +0100

    JumpKingManager done?

---
+++ WorkshopPlus/JumpKingManager/Memory.cs
@@ -0,0 +1,38 @@
+﻿using JumpKing;
+using JumpKing.GameManager;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpKingManager
+{
+    internal class Memory
+    {
+        internal static void TeleportPlayer(Screen screen, float x, float y)
+        {
+            GameLoop.m_player.m_body.Position.X = x;
+            GameLoop.m_player.m_body.Position.Y = y;
+            GameLoop.m_player.m_body.Velocity = Vector2.Zero;
+            Camera.UpdateCamera(GameLoop.m_player.m_body.GetHitbox().Center);
+        }
+
+        internal static float PlayerX()
+        {
+            return GameLoop.m_player.m_body.Position.X;
+        }
+
+        internal static float PlayerY()
+        {
+            return GameLoop.m_player.m_body.Position.Y;
+        }
+
+        internal static Screen PlayerScreen()
+        {
+            Enum.TryParse(Camera.CurrentScreen.ToString(), out Screen result);
+            return result;
+        }
+    }
+}
---
+++ WorkshopPlus/JumpKingManager/ModEntry.cs
@@ -0,0 +1,36 @@
+﻿using JumpKing.Mods;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace JumpKingManager
+{
+    [JumpKingMod("DevilSquirrel.JumpKingManager")]
+    public static class ModEntry
+    {
+        static Manager JKManager;
+
+        /// <summary>
+        /// Called by Jump King before the level loads
+        /// </summary>
+        [BeforeLevelLoad]
+        public static void BeforeLevelLoad()
+        {
+#if DEBUG
+            Debugger.Launch();
+#endif
+            try
+            {
+                System.Windows.Forms.Application.EnableVisualStyles();
+
+                JKManager = new Manager();
+                JKManager.Show();
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+    }
+}
